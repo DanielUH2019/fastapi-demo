4f419b1baac8e9bc290db6d0cbadd4e1c6750f35
DanielUH2019 <danielcardenascabrera2016@gmail.com>
2024-04-05 22:21:52 -0400

    Create tests for endpoint

---
+++ app/tests/test_solution_endpoint.py
@@ -0,0 +1,81 @@
+import math
+import pytest
+from fastapi.testclient import TestClient
+
+from app.main import api
+from app.models import Order
+
+client = TestClient(api)
+
+
+def test_solution_with_valid_orders(orders: list[Order]):
+    response = client.post(
+        "/solution",
+        json={
+            "orders": [o.model_dump() for o in orders],
+            "criterion": "completed",
+        },
+    )
+    print(response.json())
+    assert response.status_code == 200
+    assert math.isclose(response.json(), 1299.69)
+
+
+def test_solution_with_negative_price_orders():
+    orders_with_negative_price = [
+        {
+            "id": 1,
+            "item": "Laptop",
+            "quantity": 1,
+            "price": -999.99,
+            "status": "completed",
+        },
+        {
+            "id": 2,
+            "item": "Smartphone",
+            "quantity": 2,
+            "price": -499.95,
+            "status": "pending",
+        },
+        {
+            "id": 3,
+            "item": "Headphones",
+            "quantity": 3,
+            "price": -99.90,
+            "status": "completed",
+        },
+    ]
+    response = client.post(
+        "/solution", json={"orders": orders_with_negative_price, "criterion": "all"}
+    )
+    assert response.status_code == 422
+
+
+def test_solution_with_zero_quantity_orders():
+    orders_with_zero_quantity = [
+        {
+            "id": 1,
+            "item": "Laptop",
+            "quantity": 0,
+            "price": 999.99,
+            "status": "completed",
+        },
+        {
+            "id": 2,
+            "item": "Smartphone",
+            "quantity": 0,
+            "price": 499.95,
+            "status": "pending",
+        },
+        {
+            "id": 3,
+            "item": "Headphones",
+            "quantity": 0,
+            "price": 99.90,
+            "status": "completed",
+        },
+    ]
+    response = client.post(
+        "/solution", json={"orders": orders_with_zero_quantity, "criterion": "all"}
+    )
+    assert response.status_code == 422
